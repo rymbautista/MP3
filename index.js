e1132840ff94750a07cc989c7476113ee7bb9e0c
rymbautista <86502642+rymbautista@users.noreply.github.com>
2023-02-20 11:54:28 -0800

    LEts do this again

--- index.js
+++ index.js
@@ -5,41 +5,44 @@ let lives = 3;
 let gameIsOver = false;
 
 function preload() {
-    catcherImage = loadImage('gamefrog.png');
+    catcherImage = loadImage('Basket.png');
     apple = loadImage('apple.png');
   }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  catcher = new Sprite(width / 1.5, height - 80, windowWidth/10, 110);
-  catcherImage.resize(windowWidth/10, 0);
+  catcher = new Sprite(width / 1.5, height - 80, windowWidth/10, 120);
+  //catcherImage.resize(windowWidth/10, 0);
+  catcherImage.resize(0, 120);
   catcher.addImage(catcherImage);
   catcher.shapeColor = color(255, 0, 0);
 }
 
 function draw() {
-  background(220);
+  background('#4BB3FF');
   fill('white');
   rect(0, 0, windowWidth, 30, 20);
 
-//   fill(124, 252, 0);
-//   noStroke();
-//   beginShape();
-//   curveVertex(0, height);
-//   curveVertex(0, height);
-//   curveVertex(50, 250);
-//   curveVertex(100, 200);
-//   curveVertex(150, 250);
-//   curveVertex(200, 150);
-//   curveVertex(250, 200);
-//   curveVertex(300, 250);
-//   curveVertex(350, 200);
-//   curveVertex(400, height);
-//   curveVertex(400, height);
-//   endShape();
+  fill('#3E9750');
+  noStroke();
+  beginShape();
+  curveVertex(0, height);
+  curveVertex(0, height);
+  curveVertex(50, height-150);
+  curveVertex(150, height-100);
+  curveVertex(200,  height/2+50);
+  curveVertex(300,  height/2);
+  curveVertex(windowWidth/2,  height-200);
+  curveVertex(windowWidth/1.5,  height/2);
+  curveVertex(windowWidth/1.2, windowHeight);
+  curveVertex(windowWidth/1.15, height);
+  curveVertex(windowWidth-100, height);
+  curveVertex(windowWidth, height);
+  endShape();
   
 
-fill('white');
+fill('#4BB660');
+stroke(51);
   rect(0, windowHeight-30, windowWidth, 30, 20);
 
   // Move the catcher with the mouse
@@ -48,7 +51,7 @@ fill('white');
   // Spawn objects randomly
   if (frameCount % 30 == 0) {
     let object = createSprite(random([50,width /3, width /2,width /1.5 ,width-50]), 0, 30, 30);
-    apple.resize(30, 0);
+    apple.resize(40, 0);
     object.addImage(apple);
     object.velocity.y = random(2, 5);
     objects.push(object);
